da5c0c447933f272cbe5f17adcdeadbad6e4ae59
Richa77777 <121635433+Richa77777@users.noreply.github.com>
2023-02-24 13:46:28 +0300

    Update

--- IndieVarvarsJam4_Project/Assets/Core/Player/Scripts/Player.cs
+++ IndieVarvarsJam4_Project/Assets/Core/Player/Scripts/Player.cs
@@ -7,6 +7,19 @@ public class Player : MonoBehaviour
     private void Start()
     {
         _rigidBody = GetComponent<Rigidbody2D>();
+        _camera = Camera.main;
+
+        float xAxis = Mathf.Clamp(transform.position.x, _xAxisMin, _xAxisMax);
+        float yAxis = Mathf.Clamp(transform.position.y + _yOffset, _yAxisMin, _yAxisMax);
+
+        Vector3 targetPos = new Vector3(xAxis, yAxis, _camera.transform.position.z);
+
+        _camera.transform.position = targetPos;
+    }
+
+    private void Update()
+    {
+        MoveCameraToPlayer();
     }
 
     private void FixedUpdate()
@@ -15,9 +28,9 @@ public class Player : MonoBehaviour
     }
 
     #region Move
-    [Header("Player Move")]
     private Rigidbody2D _rigidBody;
 
+    [Header("Player Move")]
     [SerializeField] private float _moveSpeed;
 
     private float _horizontal;
@@ -28,11 +41,34 @@ public class Player : MonoBehaviour
 
         _rigidBody.velocity = new Vector2(_horizontal * _moveSpeed, _rigidBody.velocity.y);
     }
+    #endregion
 
-    //private void StopMove()
-    //{
-    //    _rigidBody.velocity = new Vector2(0, 0);
-    //}
+    #region CameraMove
+    private Camera _camera;
+
+    [Space(15f)]
+    [Header("Camera Move")]
+    [SerializeField] private float _cameraMoveSpeed;
+    [SerializeField] private float _yOffset;
+
+    [Space(15f)]
+
+    [SerializeField] private float _xAxisMin;
+    [SerializeField] private float _xAxisMax;
+
+    [SerializeField] private float _yAxisMin;
+    [SerializeField] private float _yAxisMax;
+
+    private void MoveCameraToPlayer()
+    {
+        float xAxis = Mathf.Clamp(transform.position.x, _xAxisMin, _xAxisMax);
+        float yAxis = Mathf.Clamp(transform.position.y + _yOffset, _yAxisMin, _yAxisMax);
+
+        Vector3 targetPos = new Vector3(xAxis, yAxis, _camera.transform.position.z);
+
+        _camera.transform.position = Vector3.Lerp(_camera.transform.position, targetPos, _cameraMoveSpeed * Time.fixedDeltaTime);
+    }
+
 
     #endregion
 
